f5d6c64eafa8ddc1960bb9111c59094c96d53ec0
rate <e145763@ie.u-ryukyu.ac.jp>
2016-08-06 13:46:31 +0900

    PID制御実装しました by akt

--- unit/Tracer.cpp
+++ unit/Tracer.cpp
@@ -20,15 +20,30 @@ void Tracer::terminate() {
 
 void Tracer::run(int target) {
 
-	const float Kp = 0.83; /* 0.63 */
-	//int target = 20;
+  const float Kp = 0.63; /* 0.63 */
+	const float Ki = 0.30;
+  const float Kd = 0.026;
 	const int bias = 0;
+  int p,i,d;
+	float integral;
 
+/* P制御
 	int diff = mColorControl->getBright() - target;
 	float turn = Kp * diff + bias;
+*/
+	/* PID制御 */
+	int diff[2];
+	diff[1] = mColorControl->getBright() - target;
+	diff[0] = diff[1];
+	integral += (diff[1] + diff[0]) / 2.0 * 0.004;
+	p = Kp * diff[1] + bias;
+	i = Ki * integral;
+	d = Kd * (diff[1] - diff[0]) / 0.004;
+	float turn = p+i+d;
+
 	mLeftWheel.setPWM(pwm - turn);
 	mRightWheel.setPWM(pwm + turn);
-	mTailWheel.setPWM(turn*10);
+	//mTailWheel.setPWM(turn*10);
 }
 
 
