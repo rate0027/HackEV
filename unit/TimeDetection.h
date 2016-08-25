c91a590d7a496f2abb950755a61b87ef0ce4a736
rate <e145763@ie.u-ryukyu.ac.jp>
2016-08-25 22:50:23 +0900

    add TimeDetection

---
+++ unit/TimeDetection.h
@@ -0,0 +1,22 @@
+#ifndef EV3_UNIT_TIMEDETECTION_H_
+#define EV3_UNIT_TIMEDETECTION_H_
+
+/* センサを使う場合はincludeが必要*/
+#include "Clock.h"
+#include "util.h"
+
+using namespace ev3api;
+
+/* クラスの宣言 */
+class TimeDetection {
+public:
+  explicit TimeDetection();
+	bool isOver(uint32_t) const;
+	void Reset();
+
+private:
+  Clock clock; 
+};
+
+
+#endif // EV3_UNIT_TIMEDETECTION_H_
